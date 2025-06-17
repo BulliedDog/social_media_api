144a9e39fe6dd4b320d08faca8d1542501ad2dcd
BulliedDog <albertorovai1@gmail.com>
2025-06-17 16:54:35 +0200

    implemented serializers to use django built in features to CRUD models

--- social_media_api/api/serializers.py
+++ social_media_api/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from social.models import *
+from social.models import CustomUser, Post, Comment
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'bio', 'profile_image']
+        fields = ['id', 'username', 'email', 'bio', 'profile_image', 'friends']
+
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = '__all__'
